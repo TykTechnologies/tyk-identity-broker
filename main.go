7e5f084e40ac50407e3b0cf82e598556e3546dfc
Leonid Bugaev <leonsbox@gmail.com>
2019-02-04 17:31:59 +0100

    Goimport fixes

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"crypto/tls"
 	"flag"
+	"net/http"
+	"path"
+	"strconv"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/TykTechnologies/tyk-identity-broker/backends"
 	"github.com/TykTechnologies/tyk-identity-broker/tap"
 	"github.com/TykTechnologies/tyk-identity-broker/tothic"
 	"github.com/TykTechnologies/tyk-identity-broker/tyk-api"
 	"github.com/gorilla/mux"
-	"net/http"
-	"path"
-	"strconv"
 )
 
 // AuthConfigStore Is the back end we are storing our configuration files to
