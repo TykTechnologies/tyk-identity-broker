f842e199e6410db9ec25b47f28855916d2cd8390
sredny buitrago <sredny.buitrago@gmail.com>
2020-02-11 16:52:24 -0500

    on redis backend, prescind of a mutex

--- backends/redis.go
+++ backends/redis.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -30,7 +29,6 @@ type RedisConfig struct {
 
 type RedisBackend struct {
 	db        redis.UniversalClient
-	dbMu      sync.RWMutex
 	config    *RedisConfig
 	KeyPrefix string
 }
@@ -100,8 +98,6 @@ func (r *RedisBackend) newRedisClusterPool() redis.UniversalClient {
 }
 
 func (r *RedisBackend) Connect() bool {
-	r.dbMu.Lock()
-	defer r.dbMu.Unlock()
 
 	r.db = r.newRedisClusterPool()
 	return true
@@ -125,12 +121,6 @@ func (r *RedisBackend) Init(config interface{}) {
 func (r *RedisBackend) SetDb(db redis.UniversalClient) {
 	r.db = db
 	redisLogger.Info("Set DB")
-}
-
-// SetDbMu set mutex from existent connection
-func (r *RedisBackend) SetDbMu(mu sync.RWMutex) {
-	r.dbMu = mu
-	redisLogger.Info("Set db mutex")
 }
 
 func (r *RedisBackend) SetKey(key string, val interface{}) error {
@@ -167,9 +157,6 @@ func (r *RedisBackend) DeleteKey(key string) error {
 }
 
 func (r *RedisBackend) getDB() redis.UniversalClient {
-	r.dbMu.RLock()
-	defer r.dbMu.RUnlock()
-
 	return r.db
 }
 
