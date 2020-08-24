58d26468b44a103ee0c952481090c07b2d5c08ef
sredny buitrago <sredny.buitrago@gmail.com>
2020-08-24 13:33:15 -0500

    fix mongo methods

--- backends/mongo.go
+++ backends/mongo.go
@@ -4,6 +4,7 @@ import (
 	"github.com/TykTechnologies/tyk-identity-broker/log"
 	"github.com/TykTechnologies/tyk-identity-broker/tap"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var mongoPrefix = "mongo-backend"
@@ -20,7 +21,7 @@ func (m MongoBackend) Init(interface{}) {
 func (m MongoBackend) SetKey(key string,value interface{}) error {
 	profilesCollection := m.Db.C(m.Collection)
 
-	profile := value.(tap.Profile)
+	profile := value.(*tap.Profile)
 	// delete if exist, where matches the profile ID and org
 	err := profilesCollection.Remove(tap.Profile{ID:key,OrgID:profile.OrgID})
 	if err != nil {
@@ -37,9 +38,7 @@ func (m MongoBackend) SetKey(key string,value interface{}) error {
 
 func (m MongoBackend) GetKey(key string, val interface{}) error {
 	profilesCollection := m.Db.C(m.Collection)
-	profile := tap.Profile{ID:key}
-
-	err := profilesCollection.Find(profile).One(&val)
+	err := profilesCollection.Find(bson.M{"ID":key}).One(val)
 	if err != nil {
 		mongoLogger.Error("error reading profiles from mongo: " + err.Error())
 	}
@@ -47,20 +46,24 @@ func (m MongoBackend) GetKey(key string, val interface{}) error {
 }
 
 func (m MongoBackend) GetAll() []interface{} {
-	var profiles []interface{}
+	var profiles []tap.Profile
 	err := m.Db.C(m.Collection).Find(nil).All(&profiles)
 	if err != nil {
 		mongoLogger.Error("error reading profiles from mongo: " + err.Error())
 	}
-	return profiles
+
+	result := make([]interface{}, len(profiles))
+	for i, profile := range profiles {
+		result[i] = profile
+	}
+
+	return result
 }
 
 func (m MongoBackend) DeleteKey(key string) error {
 	profilesCollection := m.Db.C(m.Collection)
 
-	profile := tap.Profile{ID:key}
-	err := profilesCollection.Remove(profile)
-
+	err := profilesCollection.Remove(bson.M{"ID": key})
 	if err != nil {
 		mongoLogger.WithError(err).Error("removing profile")
 	}
