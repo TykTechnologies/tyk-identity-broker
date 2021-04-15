e623d20c9c04aba7d1e60fcee944dacaa5614968
Sredny M <sredny.buitrago@gmail.com>
2021-04-15 00:16:14 -0500

    TT-1490 implemented logic to reject some sso users (#150)

--- tap/profile.go
+++ tap/profile.go
@@ -8,22 +8,23 @@ import "encoding/json"
 // combines an Action (what to do with the identity once confirmed, this is
 // delegated to an IdentityHandler) with a Provider (such as Social / GPlus)
 type Profile struct {
-	ID                    string            `bson:"ID" json:"ID"`
-	Name                  string            `bson:"Name" json:"Name"`
-	OrgID                 string            `bson:"OrgID" json:"OrgID"`
-	ActionType            Action            `bson:"ActionType" json:"ActionType"`
-	MatchedPolicyID       string            `bson:"MatchedPolicyID" json:"MatchedPolicyID"`
-	Type                  ProviderType      `bson:"Type" json:"Type"`
-	ProviderName          string            `bson:"ProviderName" json:"ProviderName"`
-	CustomEmailField      string            `bson:"CustomEmailField" json:"CustomEmailField"`
-	CustomUserIDField     string            `bson:"CustomUserIDField" json:"CustomUserIDField"`
-	ProviderConfig        interface{}       `bson:"ProviderConfig" json:"ProviderConfig"`
-	IdentityHandlerConfig interface{}       `bson:"IdentityHandlerConfig" json:"IdentityHandlerConfig"`
-	ProviderConstraints   ProfileConstraint `bson:"ProviderConstraints" json:"ProviderConstraints"`
-	ReturnURL             string            `bson:"ReturnURL" json:"ReturnURL"`
-	DefaultUserGroupID    string            `bson:"DefaultUserGroupID" json:"DefaultUserGroupID"`
-	CustomUserGroupField  string            `bson:"CustomUserGroupField" json:"CustomUserGroupField"`
-	UserGroupMapping      map[string]string `bson:"UserGroupMapping" json:"UserGroupMapping"`
+	ID                        string            `bson:"ID" json:"ID"`
+	Name                      string            `bson:"Name" json:"Name"`
+	OrgID                     string            `bson:"OrgID" json:"OrgID"`
+	ActionType                Action            `bson:"ActionType" json:"ActionType"`
+	MatchedPolicyID           string            `bson:"MatchedPolicyID" json:"MatchedPolicyID"`
+	Type                      ProviderType      `bson:"Type" json:"Type"`
+	ProviderName              string            `bson:"ProviderName" json:"ProviderName"`
+	CustomEmailField          string            `bson:"CustomEmailField" json:"CustomEmailField"`
+	CustomUserIDField         string            `bson:"CustomUserIDField" json:"CustomUserIDField"`
+	ProviderConfig            interface{}       `bson:"ProviderConfig" json:"ProviderConfig"`
+	IdentityHandlerConfig     interface{}       `bson:"IdentityHandlerConfig" json:"IdentityHandlerConfig"`
+	ProviderConstraints       ProfileConstraint `bson:"ProviderConstraints" json:"ProviderConstraints"`
+	ReturnURL                 string            `bson:"ReturnURL" json:"ReturnURL"`
+	DefaultUserGroupID        string            `bson:"DefaultUserGroupID" json:"DefaultUserGroupID"`
+	CustomUserGroupField      string            `bson:"CustomUserGroupField" json:"CustomUserGroupField"`
+	UserGroupMapping          map[string]string `bson:"UserGroupMapping" json:"UserGroupMapping"`
+	SSOOnlyForRegisteredUsers bool              `bson:"SSOOnlyForRegisteredUsers" json:"SSOOnlyForRegisteredUsers"`
 }
 
 // ProfileConstraint Certain providers can have constraints, this object sets out those constraints. E.g. Domain: "tyk.io" will limit
