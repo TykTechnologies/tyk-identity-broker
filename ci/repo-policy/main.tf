c8f36bbbd347c42b6a6b03dee1aed4676b16588c
Leonid Bugaev <leonsbox@gmail.com>
2023-03-14 15:48:06 +0300

    Fix/remove saml fork (#248) (#253)
    * Bump crewjam/saml to latest
    
    * Add missing result binding
    
    ---------
    
    Co-authored-by: Tit Petric <tit@tyk.io>
    
    (cherry picked from commit 8fdb8fad0af2ec0aa07ad2a1e1e52251e2758cf5)
    
    Co-authored-by: Tit Petric <black@scene-si.org>

---
+++ ci/repo-policy/main.tf
@@ -0,0 +1,49 @@
+terraform {
+
+  #Being used until TFCloud can be used
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Tyk"
+    workspaces {
+      name = "repo-policy-tyk-identity-broker"
+    }
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.16.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = "TykTechnologies"
+}
+
+module "tyk-identity-broker" {
+  source               = "./modules/github-repos"
+  repo                 = "tyk-identity-broker"
+  description          = "Tyk Authentication Proxy for third-party login"
+  default_branch       = "master"
+  topics                      = []
+  visibility                  = "public"
+  wiki                        = true
+  vulnerability_alerts        = true
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  release_branches     = [
+{ branch    = "master",
+	reviewers = "1",
+	convos    = "false",
+	required_tests = ["1.16"]},
+{ branch    = "release-1.2",
+	reviewers = "1",
+	convos    = "false",
+	required_tests = ["1.15"]},
+{ branch    = "release-1.3",
+	reviewers = "1",
+	convos    = "false",
+	required_tests = ["1.16"]},
+]
+}
