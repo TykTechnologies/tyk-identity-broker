a7b35ebe2b474815866260884174ee188d3bfc4c
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2023-03-16 18:27:03 -0300

    add source branch

--- repo-policy/main.tf
+++ repo-policy/main.tf
@@ -22,32 +22,33 @@ provider "github" {
 }
 
 module "tyk-identity-broker" {
-  source               = "./modules/github-repos"
-  repo                 = "tyk-identity-broker"
-  description          = "Tyk Authentication Proxy for third-party login"
-  default_branch       = "master"
+  source                      = "./modules/github-repos"
+  repo                        = "tyk-identity-broker"
+  description                 = "Tyk Authentication Proxy for third-party login"
+  default_branch              = "master"
   topics                      = []
   visibility                  = "public"
   wiki                        = true
   vulnerability_alerts        = true
   squash_merge_commit_message = "COMMIT_MESSAGES"
   squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
-  release_branches     = [
-{ branch    = "master",
-	reviewers = "1",
-	convos    = "false",
-	required_tests = ["1.16"]},
-{ branch    = "release-1.2",
-	reviewers = "0",
-	convos    = "false",
-	required_tests = ["1.15"]},
-{ branch    = "release-1.3",
-	reviewers = "0",
-	convos    = "false",
-	required_tests = ["1.16"]},
-{ branch    = "release-1.4",
-    reviewers = "0",
-    convos    = "false",
-    required_tests = ["1.16"]},
-]
+  release_branches = [
+    { branch    = "master",
+      reviewers = "1",
+      convos    = "false",
+    required_tests = ["1.16"] },
+    { branch    = "release-1.2",
+      reviewers = "0",
+      convos    = "false",
+    required_tests = ["1.15"] },
+    { branch    = "release-1.3",
+      reviewers = "0",
+      convos    = "false",
+    required_tests = ["1.16"] },
+    { branch        = "release-1.4",
+      reviewers     = "0",
+      convos        = "false",
+      source_branch = "master",
+    required_tests = ["1.16"] },
+  ]
 }
