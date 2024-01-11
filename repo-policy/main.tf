bf17e16e1918a2e8789b259ac207694a1ec31ff9
sredny buitrago <sredny.buitrago@gmail.com>
2024-01-11 11:42:31 -0300

    upgraded go version

--- repo-policy/main.tf
+++ repo-policy/main.tf
@@ -49,11 +49,11 @@ module "tyk-identity-broker" {
 { branch    = "master",
 	reviewers = "1",
 	convos    = "false",
-	required_tests = ["1.19-bullseye"]},
+	required_tests = ["1.21-bullseye"]},
 { branch    = "release-1.4",
 	reviewers = "0",
 	convos    = "false",
 	source_branch  = "master",
-	required_tests = ["1.19-bullseye"]}
+	required_tests = ["1.21-bullseye"]}
 ])
 }
