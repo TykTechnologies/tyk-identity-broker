b7c1b8f3085b114554557f2f1cf604dc61a00460
sredny buitrago <sredny@srednys-MacBook-Pro.local>
2026-04-13 15:46:11 -0400

    do not use fmt to write response

--- tap/identity-handlers/tyk_handler.go
+++ tap/identity-handlers/tyk_handler.go
@@ -404,7 +404,7 @@ func (t *TykIdentityHandler) CompleteIdentityActionForOAuth(w http.ResponseWrite
 
 		tykHandlerLogger.Info("--> No redirect, returning token...")
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, string(asJson))
+		w.Write(asJson) //nolint:errcheck
 		return
 	}
 
@@ -491,7 +491,7 @@ func (t *TykIdentityHandler) CompleteIdentityActionForTokenAuth(w http.ResponseW
 
 	tykHandlerLogger.Info("--> No redirect, returning token...")
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(asJson))
+	w.Write(asJson) //nolint:errcheck
 	return
 }
 
